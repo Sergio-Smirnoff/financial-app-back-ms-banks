8fd9715b1ef5bf79f04b46624714051108cb13c4
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2026-04-18 01:02:48 -0300

    feat(banks): bootstrap ms-banks Maven module skeleton

---
+++ src/main/java/com/financialapp/banks/BanksApplication.java
@@ -0,0 +1,18 @@
+package com.financialapp.banks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+@ConfigurationPropertiesScan
+public class BanksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanksApplication.class, args);
+    }
+}
