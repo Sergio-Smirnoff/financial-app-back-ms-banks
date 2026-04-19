2faf69f5e1e77760807d3d8982e05de6d4814de6
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2026-04-19 19:38:35 -0300

    feat(ms-banks): add InvestmentsClient feign client

---
+++ src/main/java/com/financialapp/banks/client/InvestmentsClient.java
@@ -0,0 +1,17 @@
+package com.financialapp.banks.client;
+
+import com.financialapp.banks.model.dto.response.ApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "ms-investments", url = "${investments.service.url:http://localhost:8086}")
+public interface InvestmentsClient {
+
+    @GetMapping("/api/v1/investments/holdings/valuation")
+    ApiResponse<AccountValuation> getValuation(@RequestParam("accountId") Long accountId);
+
+    record AccountValuation(Long accountId, BigDecimal totalValuation, String currency) {}
+}
